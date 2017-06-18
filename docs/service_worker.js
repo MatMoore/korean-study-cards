86b496e7e481299bacef439487c75e3cc0f9fff2
Mat Moore <matmoore@users.noreply.github.com>
2017-06-18 20:32:43 +0100

    Add a service worker with caching

---
+++ docs/service_worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('numbers-app').then(function(cache) {
+     return cache.addAll([
+       'index.html',
+       'elm.js',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(
+      function(response) {
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
